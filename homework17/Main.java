4f96ba98ae5c20b80abe9522bfb3729ef5b6b208
Petr Pyatkov <bigbox89@gmail.com>
2021-12-09 20:35:37 +0500

    Add homework 17 count words

--- homework17/Main.java
+++ homework17/Main.java
@@ -1,22 +1,36 @@
-package com.github.apppetr.javaPractice.Homework17;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * ДЗ 17. Удаление элемента по индексу
- * Реализовать метод T get(int index) для LinkedList
+ * На вход подается строка с текстом. Необходимо посчитать, сколько встречается раз каждое слово в этой строке.
+ * <p>
+ * Вывести:
+ * Слово - количество раз
+ * <p>
+ * Использовать Map, string.split(" ") - для деления текста по словам. Слово - символы, ограниченные пробелами справа и слева.
  */
 public class Main {
     public static void main(String[] args) {
-        //-------------------------------Linked List----------------------//
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(34);
-        list.add(120);
-        list.add(-10);
-        list.add(11);
-        list.add(50);
-        list.add(100);
-        list.add(99);
-        //Запрос элемента по индексу массива
-        System.out.println( list.get(1));
+        //На вход подается строка с текстом
+        String inputString = "На На вход подается строка с текстом На вход подается строка с текстом Необходимо посчитать сколько встречается раз каждое слово в этой строке";
+        Map<String, Integer> words = new TreeMap<>();
+        String[] listWords = inputString.split(" ");
+        int count = 0;
+        System.out.println("Подсчет количества повторяющихся слов в строки при помощи интерфейса Map");
+        //Делаем проход по массиву и считаем сколько раз повторяется слово.
+        //Затем добавляем все в Map с уникальными ключами
+        for (String listWord : listWords) {
+            count = 0;
+            for (String s : listWords) {
+                if (s.equals(listWord)) {
+                    count++;
+                }
+            }
+            words.put(listWord, count);
+        }
+        System.out.println(words.toString());
+
 
     }
 
